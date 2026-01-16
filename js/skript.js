610bf2eed34f26ea5f63b732dd9dd39a113aed64
Danila Shish <itluckmer@gmail.com>
2026-01-16 14:06:41 +0300

    test_rab

---
+++ js/skript.js
@@ -0,0 +1,94 @@
+console.log("----------------------")
+
+// alert("Привет")
+
+// function myfanc(){
+//     console.log("helo")
+// }
+// myfanc()
+
+// function f1(a,b){
+//     s=a+b
+//     return s
+// }
+// console.log("сложение = "+f1(2,3))
+// function f2(a,b){
+//     s=a-b
+//     return s
+// }
+// console.log("вычитание = "+ f2(3,3))
+// function f3(a,b){
+//     s=a*b
+//     return s
+// }
+// console.log("умножение = "+ f3(2,3))
+// function f4(a,b){
+//     s=a/b
+//     return s
+// }
+// console.log("Деление = "+ f4(10,2))
+
+
+// let a=30
+// let b=20
+// if (a>b){
+//     console.log("Первное число(а) больше второго") 
+// } else if (a==b){
+//     console.log("Первное число(а и b) равны")
+// }else {
+//     console.log("Первное число(b) больше Второго")
+// }
+
+// function w(z,x){
+//     if (z>x){
+//         console.log("Первное число(z) больше второго") 
+//     } else if (z==x){
+//         console.log("Первное число(z и x) равны")
+//     }else {
+//         console.log("Второе число(x) больше первного")
+//     }
+// }
+// w(1,3)
+
+
+// //функция проверки 3 чисел
+// //и расположить наименьшее и среднее и наиболшее в ответе
+// let a=3
+// let b=1
+// let c=2
+// function e(a,b,c){
+//     if (a<b&& b<c){
+//         console.log(a,b,c)
+//     } else if (a<b && c<b && a<c){
+//         console.log(a,c,b)
+//     }else if (a>b && c>b && a>c){
+//         console.log(c,b,a)
+//     }
+// }
+// e(3,1,2)
+
+// let param=3
+// switch (param){
+//     case 1:{
+//         alert(1)
+//         break
+//     }case 2:{
+//         alert(2)
+//         break
+//     }case 3:{
+//         alert(3)
+//         break
+//     }
+//     default:{
+//         alert("ВЫ ошиблись")
+//     }
+// }
+
+
+
+// ТАЙМЕР = setTimeout
+
+function show(){
+    console.log(124)
+}
+let time=setTimeout(show,2000)

4960cfe982e26823ad43a1f40840668d2a9216e9
Danila Shish <dota2mama2@gmail.com>
2026-03-24 17:16:06 +0300

    bdd

--- js/trans.js
+++ js/trans.js
@@ -1,12 +1,41 @@
+// Функция отправки запроса по id
+async function id_to_server(id, obj_dom) {
+	let url = `http://localhost/myserver/?id=${id}`
+	let response = await fetch(url, {
+		method: 'GET',
+		headers: { Accept: 'application/json' },
+	})
+
+	let param = await response.json()
+
+	obj_dom.innerHTML = param[0].text
+	obj_dom.classList.add('active')
+
+	//добавление элементов в цикле
+	// for (let key in param) {
+	// 	if (param.hasOwnProperty(key)) {
+	// 		const par = document.querySelector('.block')
+	// 		const newDiv = document.createElement('div')
+	// 		newDiv.className = 'block_inner'
+	// 		output_str = param[key].fam + param[key].name + param[key].ote
+	// 		newDiv.innerHTML = output_str
+
+	// 		par.appendChild(newDiv)
+	// 	}
+	// }
+}
+
+// Получение id по клику и отправка запроса на сервер
 ;[...document.querySelectorAll('a')].forEach((item, index) => {
 	item.addEventListener('click', e => {
-		let foo = document.querySelectorAll('.block_content')
-		for (var i = 0; i < foo.length; i++) {
-			if (foo[i].classList.contains('active')) {
-				foo[i].classList.remove('active')
-			} else {
-				foo[index].classList.add('active')
+		let id = item.dataset.id
+		let block_content = document.querySelectorAll('.block_content')
+		for (var i = 0; i < block_content.length; i++) {
+			if (block_content[i].classList.contains('active')) {
+				block_content[i].classList.remove('active')
 			}
 		}
+		//
+		id_to_server(id, block_content[index])
 	})
 })

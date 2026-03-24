91e3bc7fe1ce5f02346200e95c85618d79424773
Danila Shish <dota2mama2@gmail.com>
2026-03-24 15:32:09 +0300

    db

--- js/trans.js
+++ js/trans.js
@@ -1,23 +1,12 @@
-const blocks = document.getElementById('blocks');
-var i=0;
-console.log(blocks);
-
-blocks.addEventListener('click', click_boton)
-
-function click_boton(){
-    console.log(blocks);
-    i++;
-    if(i === 1){
-        blocks.style.backgroundColor = "rgb(223, 48, 25)";
-        blocks.style.right = "0";
-    } else if(i === 2){
-        
-        blocks.style.transform = "scale(1.2)";
-        blocks.style.backgroundColor = "rgb(116, 247, 8)";
-        blocks.style.bottom = "0";
-    } else {
-        blocks.style.transform = "rotate(50deg)";
-        blocks.style.backgroundColor = "rgb(191, 8, 247)";
-        blocks.style.left = "0"; 
-    }
-}
+;[...document.querySelectorAll('a')].forEach((item, index) => {
+	item.addEventListener('click', e => {
+		let foo = document.querySelectorAll('.block_content')
+		for (var i = 0; i < foo.length; i++) {
+			if (foo[i].classList.contains('active')) {
+				foo[i].classList.remove('active')
+			} else {
+				foo[index].classList.add('active')
+			}
+		}
+	})
+})
